36cc37df8048b10dc64b072f5e5bac72eafa15aa
Massimo <max1000.dede1000@gmail.com>
2024-05-22 22:24:57 +0200

    .

--- js/script.js
+++ js/script.js
@@ -16,9 +16,10 @@ function toggleMenu() {
   var mobileSiteMenu = document.getElementById("burger-menu-dropdown");
   var burgerMenu = document.getElementById("burger-menu")
   mobileSiteMenu.classList.toggle("show");
-  burgerMenu.style.zIndex = "2000";
+  burgerMenu.style.zIndex = "2000"; // fungerer kun på forsiden men ikke på alle de andre sider ??? Lidt hjælp? / massimo
 }
 
+// Jeg forsøgte at style det i css men det var ikke som jeg ville have, derfor prøver jeg også her med js. / Massimo
 function alignTextInMain() {
   const mainDiv = document.querySelector('main');
   mainDiv.style.textAlign = 'center';
